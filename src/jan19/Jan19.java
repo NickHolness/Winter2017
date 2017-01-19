c858700d17ef324cdc08d12489c5ecd2ae7a395d
Nick <Nick@10.250.7.41>
2017-01-19 11:26:08 -0500

    Initial Submission

---
+++ src/jan19/Jan19.java
@@ -0,0 +1,17 @@
+
+package jan19;
+
+/**
+ *
+ * @author Nick
+ */
+public class Jan19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
